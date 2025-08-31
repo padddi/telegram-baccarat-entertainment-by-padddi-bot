706094dc498cb71e8ee78ecb516d9b41d6a6247b
Patrick Bitzer <80602239+padddi@users.noreply.github.com>
2025-08-31 13:51:11 +0000

    add config.py

---
+++ confip.py
@@ -0,0 +1,13 @@
+# config.py
+DATE_FORMAT = "%d.%m.%Y"
+
+PERCENT_FORMAT = "{:.2f}".replace(".", ",")
+
+EMOJIS = {
+    "result": "✅",
+    "week": "📈",
+    "sum": "📊",
+    "month": "🗓️",
+    "year": "🗂️",
+    "refresh": "🔄"
+}
